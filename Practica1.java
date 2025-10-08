b27240e8fae1021393c3083a18507ec4dce0883b
Joan Besalduch <joanbesalduchlores@gmail.com>
2025-10-08 12:14:38 +0200

    Todo hecho

--- Practica1.java
+++ Practica1.java
@@ -6,26 +6,104 @@ public class Practica1 {
 
     //EJERCICIO 1
     public static Set<Integer> multiplos (Iterator<Integer> it) {
-        //TODO
-        return null;
+        List<Integer> numeros = new ArrayList<>();
+        while (it.hasNext()) {
+            int n = it.next();
+            if (n != 0) {
+                numeros.add(n);
+            }
+        }
+
+        Set<Integer> resultado = new HashSet<>();
+
+        for (int i = 0; i < numeros.size(); i++) {
+            int a = numeros.get(i);
+            for (int j = 0; j < numeros.size(); j++) {
+                if (i != j && a % numeros.get(j) == 0) {
+                    resultado.add(a);
+                    break;
+                }
+            }
+        }
+        return resultado;
     }
 
     //EJERCICIO2
     public static void separate (Set<Integer> cuadrados, Set<Integer> noCuadrados)  {
-        //TODO
+        Set<Integer> newCuadrados = new HashSet<>();
+
+        for (int i: cuadrados) {
+            if (cuadrados.contains(i*i) && i != 0 && i != 1) {
+                newCuadrados.add(i*i);
+            }
+            if (noCuadrados.contains(i*i)) {
+                newCuadrados.add(i*i);
+            }
+        }
+
+
+        for (int i: noCuadrados) {
+            if (noCuadrados.contains(i*i) && i != 0 && i != 1) {
+                newCuadrados.add(i*i);
+            }
+            if (cuadrados.contains(i*i)) {
+                newCuadrados.add(i*i);
+            }
+        }
+
+        noCuadrados.addAll(cuadrados);
+        noCuadrados.removeAll(newCuadrados);
+        cuadrados.clear();
+        cuadrados.addAll(newCuadrados);
 
     }
 
     //EJERCICIO 3
     public static<T> Collection<Set<T>> divideInSets (Iterator<T> it) {
-        //TODO
-        return null;
+
+        Collection<Set<T>> resultado = new ArrayList<>();
+
+        while (it.hasNext()) {
+            boolean esta = false;
+            if (resultado.isEmpty()) resultado.add(new HashSet<>());
+
+            T elemento = it.next();
+            for(Set<T> currentSet: resultado) {
+                if (!currentSet.contains(elemento)) {
+                    currentSet.add(elemento);
+                    esta = true;
+                    break;
+                }
+            }
+            if (!esta) {
+                Set<T> nuevoSet = new HashSet<>();
+                nuevoSet.add(elemento);
+                resultado.add(nuevoSet);
+            }
+
+        }
+        return resultado;
     }
 
     //EJERCICIO 4
-    public static<T> Collection<Set<T>> coverageSet2 (Set<T> u,ArrayList<Set<T>> col) {
-        //TODO
-        return null;
+    public static <T> Collection<Set<T>> coverageSet2(Set<T> u, ArrayList<Set<T>> col) {
+        for (int i = 0; i < col.size(); i++) {
+            for (int j = i + 1; j < col.size(); j++) {
+                Set<T> set1 = col.get(i);
+                Set<T> set2 = col.get(j);
+                // Skip if either set is equal to u
+                if (set1.equals(u) || set2.equals(u)) continue;
+                Set<T> union = new HashSet<>(set1);
+                union.addAll(set2);
+                if (union.equals(u)) {
+                    Collection<Set<T>> result = new HashSet<>();
+                    result.add(new HashSet<>(set1));
+                    result.add(new HashSet<>(set2));
+                    return result;
+                }
+            }
+        }
+        return new HashSet<>();
     }
 
 
